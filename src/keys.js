26ccb250d81e012ad18e34bf02926c90d9d6f201
QBO <noelonassis@gmail.com>
2024-01-02 20:33:03 -0600

    update key

--- src/keys.js
+++ src/keys.js
@@ -5,10 +5,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'roundhouse.proxy.rlwy.net',
+        host: '
         user: 'root',
-        password: 'EDEE66H3BbGH2heFd4f-ecAAAE2F-dAG',
-        database: 'railway'
+        password: '',
+        database: ''
     }
 
 };
@@ -19,10 +19,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: DB_HOST = process.env.DB_HOST || 'localhost',
-        user: DB_USER = process.env.DB_USER || 'root',
-        password: DB_PASSWORD = process.env.DB_PASSWORD || 'fnoel1995',
-        database: DB_NAME = process.env.DB_NAME || 'db_links',
+        host: DB_HOST = process.env.DB_HOST || 'xxxxx',
+        user: DB_USER = process.env.DB_USER || 'xxxx',
+        password: DB_PASSWORD = process.env.DB_PASSWORD || 'xxxxxx',
+        database: DB_NAME = process.env.DB_NAME || 'xxxx',
         port:DB_PORT = process.env.DB_PORT || 3306
     }
 
