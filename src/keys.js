080041c819219e986cf2477b61f6a580d7b87afd
QBO <noelonassis@gmail.com>
2024-01-04 18:42:02 -0600

    add icon to index page

--- src/keys.js
+++ src/keys.js
@@ -19,10 +19,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: DB_HOST = process.env.DB_HOST || 'xxxxx',
-        user: DB_USER = process.env.DB_USER || 'xxxx',
-        password: DB_PASSWORD = process.env.DB_PASSWORD || 'xxxxxx',
-        database: DB_NAME = process.env.DB_NAME || 'xxxx',
+        host: DB_HOST = process.env.DB_HOST || 'localhost',
+        user: DB_USER = process.env.DB_USER || 'root',
+        password: DB_PASSWORD = process.env.DB_PASSWORD || 'fnoel1995',
+        database: DB_NAME = process.env.DB_NAME || 'db_links',
         port:DB_PORT = process.env.DB_PORT || 3306,
         ssl: {
             rejectUnauthorized: false
