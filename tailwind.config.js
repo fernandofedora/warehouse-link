2da8e2bf8018441cfb78131d3976e7eca7998691
JohanGodinez <johang77@hotmail.com>
2024-03-27 16:25:08 -0600

    The appearance of the home screen and login wip was changed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+    },
+  },
+};
