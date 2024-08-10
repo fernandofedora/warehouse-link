fddffb8c05545925ea50e3dd7e6ba26b8fbfae83
JohanGodinez <johang77@hotmail.com>
2024-08-10 13:27:37 -0600

    redesign finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      "primary": "#9a99ff"
+    },
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
     },
